78768ba89ccdb446321d471bb3b487ae73de3996
paura-yamaguchi <paurashankou@gmail.com>
2026-05-27 15:41:46 +0900

    Add files via upload
    2-3の課題を再提出

---
+++ 2-3/Main8.java
@@ -0,0 +1,24 @@
+/**
+ * 自己紹介を表示するプログラム
+ */public class Main8 {
+    public static void main(String[] args) {
+        // 名前を保存
+        String name = "山田太郎";
+
+        // 出身地を保存
+        String hometown = "東京都";
+
+        // 好きな食べ物を保存
+        String favoriteFood = "カレーライス";
+
+        //自己紹介を表示
+        System.out.println("=====自己紹介=====");
+        System.out.println("名前: " + name);
+        System.out.println("出身地: " + hometown);
+        System.out.println("好きな食べ物: " + favoriteFood);
+
+
+        
+    }
+}
+

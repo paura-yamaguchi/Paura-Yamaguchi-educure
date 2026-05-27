5fb84287a71dfd4b1fa3b69cd0f9ec1f465083ea
paura-yamaguchi <paurashankou@gmail.com>
2026-05-27 19:56:15 +0900

    Add files via upload
    2-3の課題再々提出

--- 2-3/Main1.java
+++ 2-3/Main1.java
@@ -1,7 +1,7 @@
 public class Main1 {
     public static void main(String[] args) {
      
-        system.out.println("今日の天気：晴れ");
-        system.out.println("気温： 23.5℃");
+        System.out.println("今日の天気：晴れ");
+        System.out.println("気温： 23.5℃");
     }
 }

54d98bac4daff80fc8cf3bbeb3e3f0c241402b99
paura-yamaguchi <paurashankou@gmail.com>
2026-06-04 20:58:36 +0900

    Add files via upload
    2-10課題再提出

---
+++ 2-10/src/Main7.java
@@ -0,0 +1,35 @@
+public class Main7 {
+    public static void main(String[] args) {
+        int key = 2;
+        int pressCount = 2;
+
+        switch (key) {
+            case 1:
+                if (pressCount == 1) {
+                    System.out.println("A");
+                } else if (pressCount == 2) {
+                    System.out.println("B");
+                } else {
+                    System.out.println("C");
+                }
+                break;
+            case 2:
+                if (pressCount ==1) {
+                    System.out.println("D");
+                } else if (pressCount ==2) {
+                    System.out.println("E");
+                } else {
+                    System.out.println("F");
+                }
+                break;
+
+                default:
+                    System.out.println("Invalid key");
+                    
+        }
+    }
+}
+
+
+
+
